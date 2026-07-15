206f31362d27354730888480cb3fa02e94ff8f96
Xinyi Yang <xinyiyang175@gmail.com>
2026-07-15 23:22:04 +0800

    Add: surrounded-regions - submission-2

---
+++ Data Structures & Algorithms/surrounded-regions/submission-2.py
@@ -0,0 +1,46 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        ROWS = len(board)
+        COLS = len(board[0])
+
+
+
+
+        def capture(r,c):
+            if (r<0 or r==ROWS or c<0 or c==COLS or board[r][c] != "O"):
+                return
+            
+            board[r][c] = "T"
+
+            capture(r+1,c)
+            capture(r-1,c)
+            capture(r,c+1)
+            capture(r,c-1)
+        #1. Convert all Os to Ts
+        for r in range(ROWS):
+            for c in range(COLS):
+                #if boarder nums
+                if (board[r][c] == "O" and (r in [0, ROWS-1] or c in [0,COLS-1])):
+                    capture(r,c)
+
+
+        #2. Convert all remaining Os to Xs
+        for r in range(ROWS):
+            for c in range(COLS):
+                #if boarder nums
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+
+
+
+
+
+
+        #3. COnvert all remaining T to Os
+        for r in range(ROWS):
+            for c in range(COLS):
+                #if boarder nums
+                if board[r][c] == "T":
+                    board[r][c] = "O"
+
+        
